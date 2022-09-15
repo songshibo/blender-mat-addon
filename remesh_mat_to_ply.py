04e0e0bfa6ddf7ddc324b963657cb83f2e75f668
songshibo <stereo.bingo@gmail.com>
2022-09-15 23:43:05 +0800

    add command line remesh tool

---
+++ remesh_mat_to_ply.py
@@ -0,0 +1,24 @@
+import bpy
+import time
+
+print(bpy.app.version_string)
+bpy.ops.import_mesh.mat(filepath="E:/PROJECTS/blender-mat-addon/bug.ma")
+objects = []
+
+for obj in bpy.context.scene.objects:
+    obj.select_set(obj.type == "MESH")
+    objects.append(obj)
+
+c = {}
+c["object"] = c["active_object"] = bpy.context.object
+c["selected_objects"] = c["selected_editable_objects"] = objects
+bpy.ops.object.join(c)
+
+start_time = time.time()
+bpy.ops.object.modifier_add(type='REMESH')
+bpy.context.object.modifiers['Remesh'].mode='VOXEL'
+bpy.context.object.modifiers['Remesh'].voxel_size=0.01
+bpy.ops.object.modifier_apply(modifier='Remesh')
+print("--- %.2f seconds ---" % (time.time() - start_time))
+
+bpy.ops.export_mesh.ply(filepath="./remeshed.ply", use_selection=True)
