2e725d2914f7b3b08055e25e4763c8d01aa08888
songshibo <stereo.bingo@gmail.com>
2024-01-08 23:44:05 +0800

    change remesh python script

--- remesh_mat_to_ply.py
+++ remesh_mat_to_ply.py
@@ -2,7 +2,7 @@ import bpy
 import time
 
 print(bpy.app.version_string)
-bpy.ops.import_mesh.mat(filepath="E:/PROJECTS/blender-mat-addon/bug.ma", mat_type='std')
+bpy.ops.import_mesh.mat(filepath="D:/PROJECTS/blender-mat-addon/bug.ma", mat_type='std')
 objects = []
 
 for obj in bpy.context.scene.objects:
@@ -12,7 +12,7 @@ for obj in bpy.context.scene.objects:
 c = {}
 c["object"] = c["active_object"] = bpy.context.object
 c["selected_objects"] = c["selected_editable_objects"] = objects
-bpy.ops.object.join(c)
+bpy.ops.object.join()
 
 start_time = time.time()
 bpy.ops.object.modifier_add(type='REMESH')
@@ -21,5 +21,6 @@ bpy.context.object.modifiers['Remesh'].voxel_size=0.01
 bpy.ops.object.modifier_apply(modifier='Remesh')
 print("--- VOXEL REMESH:  %.2f seconds ---" % (time.time() - start_time))
 
-bpy.ops.export_mesh.ply(filepath="./remeshed.ply", use_selection=True)
+#bpy.ops.export_mesh.ply(filepath="./remeshed.ply", use_selection=True)
+bpy.ops.export_mesh.stl(filepath="./remeshed.stl", use_selection=True)
 bpy.ops.object.delete()
